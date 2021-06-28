f0ef51163ed78d288cc039939229b40a214da219
浅香 諒 <ryo.asaka@g.pascalia.co.jp>
2021-06-28 14:21:07 +0900

    User Signup Page

--- src/components/Auth/SignupForm.js
+++ src/components/Auth/SignupForm.js
@@ -1,43 +1,102 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useRef, useEffect, useReducer, Fragment } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Auth.module.css';
 import Button from '../UI/Button/Button';
 
 const SignupForm = () => {
-  const [formIsValid, setFormIsValid] = useState(false);
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [isShowForm, setShowForm] = useState(true);
+  const [message, setMessage] = useState(null);
+
+  const emailInput = useRef();
+  const fullnameInput = useRef();
+  const passwordInput = useRef();
+
+  const checkFromValidHandler = () => {
+    if (emailInput.current.value === "" ||
+      fullnameInput.current.value === "" ||
+      passwordInput.current.value === "") {
+      setIsFormValid(false);
+      return;
+    }
+    setIsFormValid(true);
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!isFormValid) {
+      return;
+    }
+    const signupAPI = 'http://localhost:5151/signup';
+    const signupData = {
+      email: emailInput.current.value,
+      fullname: fullnameInput.current.value,
+      fullname: passwordInput.current.value,
+    };
+    fetch(signupAPI, {
+      method: "POST",
+      headers: {
+        'Content-Typet': 'application/json',
+      },
+      body: JSON.stringify(signupData),
+    })
+      .then(response => {
+        if (response.status == 200) {
+          setMessage("Signup successfully");
+          setShowForm(false);
+          return;
+        }
+        setMessage("Signup failed, please check information!");
+      })
+      .catch((error) => {
+        setMessage(error.message);
+      });
+  }
+
 
   return (
-    <Card className={classes.auth}>
-      <form>
-        <div className={classes.control}>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-          />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="password">Full Name</label>
-          <input
-            type="text"
-            id="fullname"
-          />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-          />
-        </div>
-        <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
-            Login
-          </Button>
-        </div>
-      </form>
-    </Card>
+    <Fragment>
+      <Card className={classes.auth}>
+        {message != null && <p className={classes.error}>{message}</p>}
+        {isShowForm &&
+          <form onSubmit={submitHandler}>
+            <div className={classes.control}>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                ref={emailInput}
+                onChange={checkFromValidHandler}
+              />
+            </div>
+            <div className={classes.control}>
+              <label htmlFor="fullname">Full name</label>
+              <input
+                type="text"
+                id="fullname"
+                ref={fullnameInput}
+                onChange={checkFromValidHandler}
+              />
+            </div>
+            <div className={classes.control}>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                ref={passwordInput}
+                onChange={checkFromValidHandler}
+              />
+            </div>
+            <div className={classes.actions}>
+              <Button type="submit" className={classes.btn} disabled={!isFormValid}>
+                Signup
+              </Button>
+            </div>
+          </form>
+        }
+      </Card>
+    </Fragment>
   );
 };
 
