e70ffd0d03329a07273458599cd7c593eb003ead
浅香 諒 <ryo.asaka@g.pascalia.co.jp>
2021-06-30 13:48:54 +0900

    homework3

--- src/components/Todos/TodosList.js
+++ src/components/Todos/TodosList.js
@@ -19,7 +19,7 @@ const TodosList = () => {
             }
         })
             .then(response => {
-                setTodos(response);
+                setTodos(response.data.data);
                 setError(null);
             })
             .catch((error) => {
@@ -34,16 +34,31 @@ const TodosList = () => {
     }, []);
 
     console.dir(todos);
-
-    let todosList = <p>No Todo</p>;
-
-    if (todos.data.length !== 0) {
-        let todosList = <p>test</p>;
-    }
+    console.log(token);
 
     return (
         <ul className={classes["todos-list"]}>
-            {todosList}
+            <li className={classes.todo}>
+                <h2>Eu vix putent ceteros</h2>
+                <p>
+                    Eu vix putent ceteros. Et usu tempor perpetua, sea ludus labitur eu.
+                    Te has etiam tempor expetenda. Ex possit detracto nominati ius, vix te
+                    dicat dicam habemus, ei omnes primis omnesque ius. Eu vide erant
+                    reprimique duo. Ut doctus oporteat duo, cu eum labitur inciderint, nec
+                    illum virtute maluisset cu.
+                </p>
+            </li>
+            <li className={classes.todo}>
+                <h2>Eu vix putent ceteros</h2>
+                <p>
+                    Eu vix putent ceteros. Et usu tempor perpetua, sea ludus labitur eu.
+                    Te has etiam tempor expetenda. Ex possit detracto nominati ius, vix te
+                    dicat dicam habemus, ei omnes primis omnesque ius. Eu vide erant
+                    reprimique duo. Ut doctus oporteat duo, cu eum labitur inciderint, nec
+                    illum virtute maluisset cu.
+                </p>
+
+            </li>
         </ul>
     );
 };
